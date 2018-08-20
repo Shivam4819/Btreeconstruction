c7d4cb0edfe6d175f227a870b3343d3ef1564629
shivam4819 <shivam4819@gmail.com>
2018-08-20 10:12:21 +0530

    structure

---
+++ Node.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binaryconstruction;
+
+/**
+ *
+ * @author shivam
+ */
+public class Node {
+
+    int key;
+    Node left,right;
+
+    public Node(int data) {
+        key=data;
+        left=right=null;
+    }
+    
+}
